44c6d161d539ed9dbf2e899479ba56056c848fcd
Alex Birch <Birch-san@users.noreply.github.com>
2017-06-18 19:10:07 +0100

    make compatible with AoT compile. hm.

--- src/app/+detail/+child-detail/child-detail.states.ts
+++ src/app/+detail/+child-detail/child-detail.states.ts
@@ -1,19 +1,21 @@
 import { ChildDetailComponent } from './child-detail.component';
 import { StatesModule } from '@uirouter/angular';
 
+export function resolveAccount() {
+  return new Promise((yes, no) => {
+    setTimeout(() => {
+      yes('hey');
+    }, 500);
+  });
+}
+
 export const statesModule: StatesModule = {
   states: [
     { name: 'detail.child-detail',
       url: '/child-detail',
       component: ChildDetailComponent,
       resolve: {
-        account: () => {
-          return new Promise((yes, no) => {
-            setTimeout(() => {
-              yes('hey');
-            }, 500);
-          });
-        }
+        account: resolveAccount
       }
     }
   ]
